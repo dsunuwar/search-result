20f76f4c647bc5623008f7a25318a33a57b51e75
Dilip Sunuwar <mulicha@gmail.com>
2025-03-02 23:13:17 +1100

    chore: added constants file

---
+++ hotels/app/lib/constants.ts
@@ -0,0 +1,10 @@
+export const hotelSortOptions = {
+  highLow: {
+    text: "Price (high-low)",
+    value: "high-low",
+  },
+  lowHigh: {
+    text: "Price (low-high)",
+    value: "low-high",
+  },
+};
