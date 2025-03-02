a3f19420a76134fb4053ccb89a6aa672ad6e637d
Dilip Sunuwar <mulicha@gmail.com>
2025-03-02 23:06:18 +1100

    chore: add data and data type definitions

--- hotels/app/lib/definitions.ts
+++ hotels/app/lib/definitions.ts
@@ -1,11 +1,22 @@
 // keep all data type definitions here
+export enum RatingTypeEnum {
+  self = "self",
+  star = "star",
+}
 
-type Rating = {
+// Possible union types for usage
+type RatingType = "self" | "star";
+type ImageType = "PRIMARY";
+type PromotionTypes = "MEMBER" | "CAMPAIGN";
+
+// --------- Property
+
+export type RatingT = {
   ratingType: string;
   ratingValue: number;
 };
 
-type PreviewImage = {
+export type PreviewImage = {
   caption: string;
   imageType: string;
   url: string;
@@ -16,5 +27,41 @@ export type Property = {
   title: string;
   address: string[];
   previewImage: PreviewImage;
-  rating: Rating;
+  rating: RatingT;
+};
+
+export type HotelsT = {
+  id: string;
+  property: Property;
+  offer: OfferT;
+};
+
+// --------- Offer
+
+export type PromotionT = {
+  title: string;
+  type?: string;
+};
+
+export enum CancellationType {
+  notRefundable = "NOT_REFUNDABLE",
+  freeCancellation = "FREE_CANCELLATION",
+}
+
+export type OfferT = {
+  cancellationOption: {
+    cancellationType: string;
+  };
+
+  displayPrice: {
+    amount: number;
+    currency: string;
+  };
+
+  name: string;
+  promotion: PromotionT;
+  saving?: {
+    amount: number;
+    currency: string;
+  };
 };
