0d88c3a919ac2a26438f83b8f67715bea3098267
Dilip Sunuwar <mulicha@gmail.com>
2025-03-03 11:07:53 +1100

    test: added unit test for sorting function

--- hotels/app/lib/utils.spec.ts
+++ hotels/app/lib/utils.spec.ts
@@ -1,11 +1,42 @@
-"use client";
-
 import { sortHotelsBy } from "@/app/lib/utils";
 import { hotelSortOptions } from "@/app/lib/constants";
-import { describe } from "node:test";
+
+import { searchResults } from "@/app/lib/data";
+import { HotelsT } from "./definitions";
 
 describe("sortHotelsBy", () => {
+  // clone data to be safe from accidental mutation
+  const cloneHotelsData: HotelsT[] = JSON.parse(
+    JSON.stringify(searchResults.results),
+  );
+
+  const lastItem = cloneHotelsData.length - 1;
+
+  it("returns empty when hotel results empty", () => {
+    const sortResultHighLow = sortHotelsBy([], hotelSortOptions.highLow.value);
+    expect(sortResultHighLow).toEqual([]);
+  });
+
   it("sorts hotel by Price (high-low)", () => {
-    sortHotelsBy([], hotelSortOptions.highLow.value);
+    const sortResultHighLow = sortHotelsBy(
+      cloneHotelsData,
+      hotelSortOptions.highLow.value,
+    );
+    // first value to be high
+    expect(sortResultHighLow[0].offer.displayPrice.amount).toEqual(535);
+    // last value to be low
+    expect(sortResultHighLow[lastItem].offer.displayPrice.amount).toEqual(227);
+  });
+
+  it("sorts hotel by Price (low-high)", () => {
+    const sortResultHighLow = sortHotelsBy(
+      cloneHotelsData,
+      hotelSortOptions.lowHigh.value,
+    );
+
+    // first value to be low
+    expect(sortResultHighLow[0].offer.displayPrice.amount).toEqual(227);
+    // last value to be high
+    expect(sortResultHighLow[lastItem].offer.displayPrice.amount).toEqual(535);
   });
 });
