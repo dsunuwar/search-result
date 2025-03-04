449be645023eb931ed7cb971431efab39332274c
Dilip Sunuwar <mulicha@gmail.com>
2025-03-04 12:11:44 +1100

    feat: updated rating component to use icons

---
+++ hotels/app/ui/rating.spec.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+describe("Rating", () => {
+  it("renders stars matching rating value", () => {});
+  it("renders circles matching rating value", () => {});
+});
--- hotels/app/ui/rating.tsx
+++ hotels/app/ui/rating.tsx
@@ -1,14 +1,100 @@
 "use client";
 
 import { RatingTypeEnum, type RatingT } from "@/app/lib/definitions";
+import { Fragment } from "react";
+import {
+  FaStar,
+  FaStarHalfAlt,
+  FaRegStar,
+  FaCircle,
+  FaRegCircle,
+} from "react-icons/fa";
+import { FaCircleHalfStroke } from "react-icons/fa6";
 
-/**
- * TODO: Create rating component or use existing lib
- */
+type RateProp = {
+  index: number;
+  activeStar: number;
+  color: string;
+};
+
+const StarRate = ({ index, activeStar, color }: RateProp) => {
+  if (index < activeStar && index + 1 < activeStar) {
+    return (
+      <>
+        <FaStar color={color} />
+      </>
+    );
+  }
+
+  if (index < activeStar && activeStar < index + 1 && activeStar % 1 !== 0) {
+    return (
+      <>
+        <FaStarHalfAlt color={color} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <FaRegStar color={color} />
+    </>
+  );
+};
+
+const CircleRate = ({ index, activeStar, color }: RateProp) => {
+  if (index < activeStar) {
+    return (
+      <>
+        <FaCircle color={color} />
+      </>
+    );
+  }
+
+  if (index < activeStar && activeStar < index + 1 && activeStar % 1 !== 0) {
+    return (
+      <>
+        <FaCircleHalfStroke color={color} />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <FaRegCircle color={color} />
+    </>
+  );
+};
+
 export default function Rating({ ratingType, ratingValue }: RatingT) {
-  return ratingType === RatingTypeEnum.star ? (
-    <>*:{ratingValue}</>
-  ) : (
-    <>o:{ratingValue}</>
+  const totalStars = 5;
+  const activeStars = ratingValue;
+
+  return (
+    <span
+      style={{
+        display: "flex",
+      }}
+    >
+      {[...new Array(totalStars)].map((item, index) => {
+        return (
+          <Fragment key={index}>
+            {ratingType === RatingTypeEnum.star ? (
+              <StarRate
+                index={index}
+                activeStar={activeStars}
+                color="#FBCB3B"
+              />
+            ) : (
+              <CircleRate
+                index={index}
+                activeStar={activeStars}
+                color="#FBCB3B"
+              />
+            )}
+          </Fragment>
+        );
+      })}
+      {activeStars}
+    </span>
   );
 }
