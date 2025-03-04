46b8f3d658dc2dfce831793a3e03b429b259f349
Dilip Sunuwar <mulicha@gmail.com>
2025-03-04 23:16:08 +1100

    feat: added logo and adusted page width

--- hotels/app/page.tsx
+++ hotels/app/page.tsx
@@ -1,13 +1,31 @@
 import SearchResult from "@/app/ui/search-result/cards";
 import { searchResults } from "@/app/lib/data";
+import Image from "next/image";
+import logoImg from "../public/logo.png";
 
 export default function Home() {
   // assuming this will be dynamic value
   const city = "Sydney";
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center">
+    <div
+      style={{
+        maxWidth: "860px",
+        margin: "15px auto",
+      }}
+    >
+      <header
+        style={{
+          padding: "5px",
+        }}
+      >
+        <Image
+          src={logoImg}
+          alt="logo"
+          style={{ width: "170px", height: "100%" }}
+        />
+      </header>
+      <main>
         <SearchResult results={searchResults.results} city={city} />
       </main>
     </div>
